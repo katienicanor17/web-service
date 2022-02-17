012b6e7ee0ba272b900d86789a4ea791090583ae
katienicanor17 <nicanorkatie17@gmail.com>
2022-02-17 11:01:04 -0800

    Add ds replit files

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title> PHP Page </title>
+</head>
+<body>
+    <h1>PHP page!</h1>
+    <p>This is HTML</p>
+    <?php
+        echo "<p>this is php</p>"
+    ?>
+    <p>This is HTML</p>
+
+</body>
+</html>
